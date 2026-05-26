37d2ba9492add1ed210328bf189f1828f1c3e221
Shalama <shalamafadillaa@gmail.com>
2026-05-26 16:22:13 +0700

    Create routes array

---
+++ src/components/routes/routes.jsx
@@ -0,0 +1,15 @@
+const routes = [
+    {
+        path: '/',
+        element: <HomePage />,
+        errorElement: <ErrorPage />  
+    },
+    {
+        path: '/shop-page',
+        element: <ShopPage />
+    },
+    {
+        path: '/cart-page',
+        element: <CartPage />
+    }
+]
