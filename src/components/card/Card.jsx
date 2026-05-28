fbcb6ff2ceb5d195abae9a2d9ade44c4a2d917f8
Shalama <shalamafadillaa@gmail.com>
2026-05-28 20:12:46 +0700

    Create handleQuantityInput function
    Create to handle the user input on input field for item's quantity.

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+export default function Card({item}) {
+    const [quantity, setQuantity] = useState(1);
+
+    function handleQuantityInput(e) {
+        const num = Math.floor(Number(e.target.value));
+
+        if (isNaN(num)) {
+            setQuantity(1);
+            return;
+        }
+
+        if (num < 0 || num === 0) {
+            setQuantity(1);
+        } else {
+            setQuantity(num);
+        }
+    }
+
+
+    return (
+        <article className="card">
+            <div className="img-item-container">
+                <img src="" alt="An image of a product item" />
+            </div>
+            <h3>{item.name}</h3>
+            <div className="quantity-section">
+                <div className="input-field-container">
+                    <label className ="label-quantity" htmlFor="quantity">{quantity}</label>
+                    <input type="number" name="quantity" className ="input-quantity"/>
+                </div>
+                <div className="quantity-button-container">
+                    <button className="increment button">+</button>
+                    <button className="decrement button">-</button>
+                </div>
+            </div>
+            <div className="add-to-cart-button-container">
+                <button className="add-to-cart-button">Add To Cart</button>
+            </div>
+        </article>
+    )
+}
