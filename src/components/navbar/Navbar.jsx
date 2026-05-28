9517cc72fa88376592bbd881f1d26ce2dd5a7a05
Shalama <shalamafadillaa@gmail.com>
2026-05-28 15:48:02 +0700

    Create Navbar component

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router"
+
+export default function Navbar() {
+    return (
+        <nav className="navlink-container">
+            <NavLink className='navlink home' to='/'>Home</NavLink>
+            <NavLink className='navlink shop' to='/shop-page'>Shop</NavLink>
+            <NavLink className='navlink cart' to='/cart-page'>Cart</NavLink>
+        </nav>
+    )
+}
