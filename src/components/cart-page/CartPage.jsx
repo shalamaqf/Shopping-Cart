292916af16e9a5195911d1175127abff0fbbbc30
Shalama <shalamafadillaa@gmail.com>
2026-05-29 19:59:50 +0700

    Create calculateTotalPrice function and jsx
    Create it to calculate total price of the cart.

--- src/components/cart-page/CartPage.jsx
+++ src/components/cart-page/CartPage.jsx
@@ -1,10 +1,25 @@
 import ItemGrid from '../item-grid/ItemGrid.jsx'
 
 export default function CartPage() {
+
+    function calculateTotalPrice() {
+        let total = 0;
+
+        for (let i = 0; i < cartItems.length; i++) {
+            const subtotal = cartItems[i].price * cartItems[i].quantity;
+            total += subtotal;
+        }
+        
+        return total;
+    }
+
     return (
         <main className='cart-page'>
             <h2>Cart</h2>
             <ItemGrid />
+            <div className='total-container'>
+                <p className='total'>Total: {calculateTotalPrice()}</p>
+            </div>
         </main>
     )
 }
