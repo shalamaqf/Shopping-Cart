fbae7a25cdaffbe327ca44e804cbb32036e570c5
Shalama <shalamafadillaa@gmail.com>
2026-05-29 17:25:54 +0700

    Create CartPage component

---
+++ src/components/cart-page/CartPage.jsx
@@ -0,0 +1,10 @@
+import ItemGrid from '../item-grid/ItemGrid.jsx'
+
+export default function CartPage() {
+    return (
+        <main className='cart-page'>
+            <h2>Cart</h2>
+            <ItemGrid />
+        </main>
+    )
+}
