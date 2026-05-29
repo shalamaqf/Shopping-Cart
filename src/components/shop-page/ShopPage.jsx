fa5165fffc9b64522810971f196b5e212997e23b
Shalama <shalamafadillaa@gmail.com>
2026-05-29 14:58:54 +0700

    Create product state in shop page

---
+++ src/components/shop-page/ShopPage.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+import { PRODUCT_API_URL } from '.../data/api.js';
+
+export default function ShopPage() {
+    const [product, setProduct] = useState([]);
+
+    async function fetchProductData() {
+        try {
+            const responses = await fetch(PRODUCT_API_URL);
+            if (!responses.ok) throw new Error ('HTTP Error');
+            const data  = await responses.json();
+            return data;
+        } catch {
+            console.log('Fetch failed');
+            return [];
+        }
+    }
+
+    return (
+        
+    )
+}
