6c31d150911fe35d58ae82576a9cad2ffdbac80f
Shalama <shalamafadillaa@gmail.com>
2026-06-01 16:51:07 +0700

    Create incrementItemQuantity function
    Create to increment item's quantity by 1 when user click increment button.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -31,4 +31,20 @@ export default function App() {
             prevCart.filter(item => item.id !== newItem.id)
         )
     }
+
+    function incrementItemQuantity(newItem) {
+        setCart(prevCart =>{
+            const isExist = prevCart.find(item => item.id === newItem.id);
+
+            if (isExist) {
+                return prevCart.map(item => 
+                    item.id === newItem.id ?
+                    {...item, quantity: item.quantity + 1} :
+                    item
+                )
+            } else {
+                return prevCart;
+            }
+        })
+    }
 }
