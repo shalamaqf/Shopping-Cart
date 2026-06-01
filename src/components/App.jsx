2eec11380275df6afd3a204388db779d9d2d5227
Shalama <shalamafadillaa@gmail.com>
2026-06-01 16:22:10 +0700

    Create addToCart function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,28 @@
 export default function App() {
     const [cart, setCart] = useState([]);
+
+
+    function addToCart(newItem, newQuantity) {
+        const newObj = {
+            id: newItem.id,
+            name: newItem.name,
+            quantity: newQuantity,
+            price: newItem.price
+        }
+
+        setCart(prevCart => {
+                const isExist = prevCart.find(item => item.id === newObj.id);
+
+                if (isExist) {
+                    return prevCart.map(item => 
+                        item.id === newObj.id ? 
+                        {...item, quantity: item.quantity + newObj.quantity} : 
+                        item
+                    )
+                } else {
+                    return [...prevCart, newObj];
+                }
+            }
+        )
+    }
 }
