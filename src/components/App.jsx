2ee4e3003f6f77972826da48e459f9f3268bec0d
Shalama <shalamafadillaa@gmail.com>
2026-06-01 17:20:53 +0700

    Create resetQuantityToDefault function
    Create to reset the item's quantity to 1 when user cancel the removal of the item.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -63,4 +63,20 @@ export default function App() {
             }
         })
     }
+
+    function resetQuantityToDefault(newItem) {
+        setCart(prevCart => {
+            const isExist = prevCart.find(item => item.id === newItem.id);
+
+            if (isExist) {
+                return prevCart.map(item =>
+                    item.id === newItem.id ?
+                    {...item, quantity: 1} :
+                    item
+                )
+            } else {
+                return prevCart;
+            }
+        })
+    }
 }
