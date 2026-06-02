5852395a01d6d7a8c6d4c7a4af3924a29d7965ad
Shalama <shalamafadillaa@gmail.com>
2026-06-02 15:49:36 +0700

    Pass props to jsx element in Item component

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -10,8 +10,8 @@ export default function Item({item, removeItem, incrementItemQuantity, decrement
             <div className="removal-container">
                 <p className="removal-permission-text">Are you sure want to remove {item.name} from the cart?</p>
                 <div className="removal-button-container">
-                    <button className="yes-button">Yes</button>
-                    <button className="no-button">No</button>
+                    <button className="yes-button" onClick={() => removeItem(item)}>Yes</button>
+                    <button className="no-button" onClick={() => resetQuantityToDefault(item)}>No</button>
                 </div>
             </div>
             :
@@ -22,8 +22,8 @@ export default function Item({item, removeItem, incrementItemQuantity, decrement
                     <p className="cart-item-detail subtotal">Subtotal: {item.price * item.quantity}</p>
                 </div>
                 <div className="item-cart-button-container">
-                    <button className="increment-cart">+</button>
-                    <button className="decrement-cart">-</button>
+                    <button className="increment-cart" onClick={() => incrementItemQuantity(item)}>+</button>
+                    <button className="decrement-cart" onClick={() => decrementItemQuantity(item)}>-</button>
                 </div>
             </div>
         }
