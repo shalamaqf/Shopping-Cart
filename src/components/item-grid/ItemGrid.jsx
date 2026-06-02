1e0637656ce417b4bbdc248dfa0cd4c2409cb958
Shalama <shalamafadillaa@gmail.com>
2026-06-02 15:07:35 +0700

    Add prop validator for ItemGrid component

--- src/components/item-grid/ItemGrid.jsx
+++ src/components/item-grid/ItemGrid.jsx
@@ -1,4 +1,5 @@
 import Item from '../item/Item.jsx'
+import PropTypes from 'prop-types'
 
 export default function ItemGrid({removeItem, incrementItemQuantity, decrementItemQuantity, resetQuantityToDefault, cart}) {
     return (
@@ -10,4 +11,19 @@ export default function ItemGrid({removeItem, incrementItemQuantity, decrementIt
             }
         </div>
     )
+}
+
+ItemGrid.propTypes = {
+    removeItem: PropTypes.func.isRequired,
+    incrementItemQuantity: PropTypes.func.isRequired,
+    decrementItemQuantity: PropTypes.func.isRequired,
+    resetQuantityToDefault: PropTypes.func.isRequired,
+    cart: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            name: PropTypes.string.isRequired,
+            quantity: PropTypes.number.isRequired,
+            price: PropTypes.number.isRequired
+        }).isRequired
+    ).isRequired
 }
