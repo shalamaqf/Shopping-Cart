fe07438a81d88761afb56faffc5bfdbe78f9126a
Shalama <shalamafadillaa@gmail.com>
2026-06-03 15:54:53 +0700

    Refactor the test

--- src/components/home-page/HomePage.test.jsx
+++ src/components/home-page/HomePage.test.jsx
@@ -3,10 +3,7 @@ import { render, screen } from "@testing-library/react";
 import { createMemoryRouter, MemoryRouter, RouterProvider } from "react-router-dom";
 import HomePage from './HomePage.jsx';
 import userEvent from "@testing-library/user-event";
-import App from '../App.jsx'
-import ShopPage from "../shop-page/ShopPage.jsx";
-import ErrorPage from '../error-page/ErrorPage.jsx'
-import CartPage from '../cart-page/CartPage.jsx'
+import { routes } from '../routes/routes.jsx'
 
 describe('HomePage component', () => {
     it('Render HomePage component to match snapshot', () => {
@@ -21,23 +18,9 @@ describe('HomePage component', () => {
     it("Go to ShopPage if user click on Link 'Shop Now", async () => {
         // Setup test
         const user = userEvent.setup();
-        const routes = createMemoryRouter(
-                [
-                    {
-                        path: '/',
-                        element: <App />,
-                        errorElement: <ErrorPage />,
-                        children: [
-                            { index: true, element: <HomePage /> },
-                            { path: 'shop-page', element: <ShopPage /> },
-                            { path: 'cart-page', element: <CartPage /> }, // optional for this test
-                        ],
-                    },
-                ],
-            { initialEntries: ['/'] }
-        );
+        const router = createMemoryRouter(routes, { initialEntries: ['/'] });
 
-        render(<RouterProvider router={routes} />);
+        render(<RouterProvider router={router} />);
         expect(screen.getByText('Shop now')).toBeInTheDocument();
         
         const link = screen.getByRole('link', { name: 'Shop now'});
