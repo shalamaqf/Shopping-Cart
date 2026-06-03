d202a8bf36ede1ef7794e38688d05d15dd729f55
Shalama <shalamafadillaa@gmail.com>
2026-06-03 16:20:55 +0700

    Test if home page appears
    Test if home page appears when user click 'Home' link inside the navbar.

---
+++ src/components/navbar/Navbar.test.jsx
@@ -0,0 +1,20 @@
+import userEvent from "@testing-library/user-event";
+import { createMemoryRouter, RouterProvider } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import { routes } from "../routes/routes";
+
+describe('Navbar component', () => {
+    it("Go to home page if user click on 'Home' link", async () => {
+        const user = userEvent.setup();
+        const router = createMemoryRouter(routes, { initialEntries: ['/'] });
+
+        render(<RouterProvider router={router} />);
+
+        const homeLink = screen.getByRole('link', { name: 'Home'});
+
+        await user.click(homeLink);
+
+        expect(screen.getByText('Shop now')).toBeInTheDocument();
+    })
+})
