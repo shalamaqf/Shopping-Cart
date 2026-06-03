135a4133f5459c8d038e1a2f7624ee6f0700db89
Shalama <shalamafadillaa@gmail.com>
2026-06-03 15:19:19 +0700

    Test if shop page is appear when user click Link

--- src/components/home-page/HomePage.test.jsx
+++ src/components/home-page/HomePage.test.jsx
@@ -1,7 +1,12 @@
 import { describe, it, expect } from "vitest";
-import { render } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import { createMemoryRouter, MemoryRouter, RouterProvider } from "react-router-dom";
 import HomePage from './HomePage.jsx';
+import userEvent from "@testing-library/user-event";
+import App from '../App.jsx'
+import ShopPage from "../shop-page/ShopPage.jsx";
+import ErrorPage from '../error-page/ErrorPage.jsx'
+import CartPage from '../cart-page/CartPage.jsx'
 
 describe('HomePage component', () => {
     it('Render HomePage component to match snapshot', () => {
@@ -11,5 +16,34 @@ describe('HomePage component', () => {
             </MemoryRouter>
         )
         expect(container).toMatchSnapshot();
+    })
+
+    it("Go to ShopPage if user click on Link 'Shop Now", async () => {
+        // Setup test
+        const user = userEvent.setup();
+        const routes = createMemoryRouter(
+                [
+                    {
+                        path: '/',
+                        element: <App />,
+                        errorElement: <ErrorPage />,
+                        children: [
+                            { index: true, element: <HomePage /> },
+                            { path: 'shop-page', element: <ShopPage /> },
+                            { path: 'cart-page', element: <CartPage /> }, // optional for this test
+                        ],
+                    },
+                ],
+            { initialEntries: ['/'] }
+        );
+
+        render(<RouterProvider router={routes} />);
+        expect(screen.getByText('Shop now')).toBeInTheDocument();
+        
+        const link = screen.getByRole('link', { name: 'Shop now'});
+
+        await user.click(link);
+
+        expect(screen.getByText('Products')).toBeInTheDocument();
     })
 })
