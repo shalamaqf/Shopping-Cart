1170effae7be5f02c4c07f9387ae3e28ef4345b9
Shalama <shalamafadillaa@gmail.com>
2026-06-04 17:39:53 +0700

    Test if shop page display nothing if fetch failed

--- src/components/shop-page/ShopPage.test.jsx
+++ src/components/shop-page/ShopPage.test.jsx
@@ -39,4 +39,27 @@ describe('Shop page component', () => {
         expect(screen.getByText('Sandals')).toBeInTheDocument();
         expect(global.fetch).toHaveBeenCalledWith('https://fakestoreapi.com/products?_limit=6')
     })
+
+    it('Display error if the fetch is failed', () => {
+        const mockProducts = [
+            { id: 1, title: 'Jacket', image: 'jacket.jpg', price: 79.9 },
+            { id: 2, title: 'Shirt', image: 'shirt.jpg', price: 49.9 },
+            { id: 3, title: 'Hat', image: 'hat.jpg', price: 39.9 },
+            { id: 4, title: 'Jeans', image: 'jeans.jpg', price: 69.9 },
+            { id: 5, title: 'Boots', image: 'boots.jpg', price: 59.9 },
+            { id: 6, title: 'Sandals', image: 'sandals.jpg', price: 49.9 }
+        ]
+        const mockResponse = {
+            ok: false,
+            json: async () => mockProducts,
+        }
+
+        global.fetch.mockResolvedValue(mockResponse);
+
+        const router = createMemoryRouter(routes, { initialEntries: ['/shop-page'] });
+        render(<RouterProvider router={router} />);
+
+        expect(screen.queryAllByRole('article')).toHaveLength(0);
+
+    })
 })
