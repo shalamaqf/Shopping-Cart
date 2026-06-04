ae0c3ee2b5d1187d476a21a0d26213231dcb733b
Shalama <shalamafadillaa@gmail.com>
2026-06-04 15:16:44 +0700

    Test if shop page shows 6 products correctly

---
+++ src/components/shop-page/ShopPage.test.jsx
@@ -0,0 +1,42 @@
+import { createMemoryRouter, RouterProvider } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { routes } from "../routes/routes";
+
+
+describe('Shop page component', () => {
+    beforeEach(() => {
+        global.fetch = vi.fn();
+    });
+
+
+    it('Show 6 products', async () => {
+        const mockProducts = [
+            { id: 1, title: 'Jacket', image: 'jacket.jpg', price: 79.9 },
+            { id: 2, title: 'Shirt', image: 'shirt.jpg', price: 49.9 },
+            { id: 3, title: 'Hat', image: 'hat.jpg', price: 39.9 },
+            { id: 4, title: 'Jeans', image: 'jeans.jpg', price: 69.9 },
+            { id: 5, title: 'Boots', image: 'boots.jpg', price: 59.9 },
+            { id: 6, title: 'Sandals', image: 'sandals.jpg', price: 49.9 }
+        ]
+        const mockResponse = {
+            ok: true,
+            json: async () => mockProducts,
+        }
+
+        global.fetch.mockResolvedValue(mockResponse);
+
+        const router = createMemoryRouter(routes, { initialEntries: ['/shop-page'] });
+        render(<RouterProvider router={router} />);
+
+        await screen.findByText('Products');
+        expect(screen.getAllByRole('article')).toHaveLength(6);
+        expect(screen.getByText('Jacket')).toBeInTheDocument();
+        expect(screen.getByText('Shirt')).toBeInTheDocument();
+        expect(screen.getByText('Hat')).toBeInTheDocument();
+        expect(screen.getByText('Jeans')).toBeInTheDocument();
+        expect(screen.getByText('Boots')).toBeInTheDocument();
+        expect(screen.getByText('Sandals')).toBeInTheDocument();
+        expect(global.fetch).toHaveBeenCalledWith('https://fakestoreapi.com/products?_limit=6')
+    })
+})
