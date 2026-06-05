f342a26cfa03e2fc557e20328d1dea525107899a
Shalama <shalamafadillaa@gmail.com>
2026-06-05 16:54:20 +0700

    Test no button on removal in Item component
    Test if user click no on removal, it will set item's quantity to default (1).

--- src/components/item/Item.test.jsx
+++ src/components/item/Item.test.jsx
@@ -180,4 +180,52 @@ describe('Item component', () => {
 
         expect(cartItem).toHaveLength(1);
     })
+    
+    it("Test if user click no on removal, it will set item's quantity to default (1)", async () => {
+        // Setup
+        const user = userEvent.setup();
+
+        const mockProducts = [
+            { id: 1, title: 'Jacket', image: 'jacket.jpg', price: 79.9 },
+            { id: 2, title: 'Shirt', image: 'shirt.jpg', price: 49.9 },
+            { id: 3, title: 'Hat', image: 'hat.jpg', price: 39.9 },
+            { id: 4, title: 'Jeans', image: 'jeans.jpg', price: 69.9 },
+            { id: 5, title: 'Boots', image: 'boots.jpg', price: 59.9 },
+            { id: 6, title: 'Sandals', image: 'sandals.jpg', price: 49.9 }
+        ]
+        const mockResponse = {
+            ok: true,
+            json: async () => mockProducts,
+        }
+
+        global.fetch.mockResolvedValue(mockResponse);
+
+        const router = createMemoryRouter(routes, { initialEntries: ['/'] });
+        const { container } = render(<RouterProvider router={router} />);
+
+        // Implement user flow
+        const shopLink = screen.getByRole('link', { name: 'Shop'});
+        await user.click(shopLink);
+
+        const addToCartButton1 = screen.getAllByRole('button', { name: 'Add To Cart'})[0];
+        const addToCartButton2 = screen.getAllByRole('button', { name: 'Add To Cart'})[1];
+        await user.click(addToCartButton1);
+        await user.click(addToCartButton2);
+
+        const cartLink = screen.getByRole('link', { name: 'Cart'});
+        await user.click(cartLink);
+
+        const incrementButton = screen.getAllByRole('button', { name: '+'})[1];
+        await user.click(incrementButton)
+
+        const decrementButton = screen.getAllByRole('button', { name: '-'})[0];
+        await user.click(decrementButton);
+
+        const noButton = screen.getByRole('button', { name: 'No'});
+        await user.click(noButton);
+        const cartItem = container.querySelectorAll('.item');
+
+        expect(cartItem).toHaveLength(2);
+        expect(screen.getByText('Quantity: 1')).toBeInTheDocument();
+    })
 })
