f7eb16818dd9988d05824fc866cdd5c834f87fda
Shalama <shalamafadillaa@gmail.com>
2026-06-09 14:14:33 +0700

    Change jsx elements return for home page
    Make the img as the background of main element.

--- src/components/home-page/HomePage.jsx
+++ src/components/home-page/HomePage.jsx
@@ -1,19 +1,15 @@
 import { Link } from "react-router-dom"
-import bg from '../../assets/background.jpg'
 import styles from './HomePage.module.css'
 
 export default function HomePage() {
     return (
         <main className={styles.main}>
-            <div className={styles['image-container']}>
-                <img className={styles.image} src={bg} alt="Hero image of a girl with a long sleeve shirt"/>
-            </div>
             <div className={styles.desc}>
                 <h1>Shop the Best</h1>
-                <p>Find what you love. Great prices, reliable delivery, and a seamless shopping experience.</p>
+                <p>Find what you love. <br />Great prices, reliable delivery, and a seamless shopping experience.</p>
             </div>
             <div className={styles.link}>
-                <Link className='shop-now-link' to='shop-page'>Shop now</Link>
+                <Link className='shop-now-link' to='shop-page'>🛍️ Shop now</Link>
             </div>
         </main>
     )
