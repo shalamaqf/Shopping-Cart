2979eee726c3c0687097df7b590c97c793561e0c
Shalama <shalamafadillaa@gmail.com>
2026-06-09 14:51:59 +0700

    Style the HomePage component

--- src/components/home-page/HomePage.jsx
+++ src/components/home-page/HomePage.jsx
@@ -3,13 +3,13 @@ import styles from './HomePage.module.css'
 
 export default function HomePage() {
     return (
-        <main className={styles.main}>
+        <main>
             <div className={styles.desc}>
                 <h1>Shop the Best</h1>
                 <p>Find what you love. <br />Great prices, reliable delivery, and a seamless shopping experience.</p>
             </div>
             <div className={styles.link}>
-                <Link className='shop-now-link' to='shop-page'>🛍️ Shop now</Link>
+                <Link className='shop-now-link' to='shop-page'>➔ Shop now</Link>
             </div>
         </main>
     )
